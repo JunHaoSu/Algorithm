05dd751fe73b9a89710f8841fa36563281582c3a
SuJunHao <sujunhao0504@gmail.com>
2017-11-15 14:11:53 +0800

    游艇租用问题

--- rent.cpp
+++ rent.cpp
@@ -4,6 +4,8 @@
 	> Mail: sujunhao0504@gmail.com
 	> Created Time: 2017年11月14日 星期二 19时15分51秒
  ************************************************************************/
+/* 该算法的思想是,如果单步租用小于连续租用的钱，就用单步租用的钱替换掉连续租用的钱。即本例的1站->2站->3站的租金替换掉1站->3站的租金。依次循环，最后右上角的那个值就是最小的租金。
+ */
 #include<iostream>  
 #include<fstream>
 #include<vector>
@@ -19,7 +21,7 @@ int R(int n){
         for (i=0;i<n-k;i++){
             int mark=i+k;
             for(j=i+1;j<mark;j++){
-                if(list[i][j]+list[j][mark]<list[i][mark]){
+                if(list[i][j]+list[j][mark]<list[i][mark]){//核心代码
                 list[i][mark]=list[i][j]+list[j][mark];
             }  
         }  
@@ -35,7 +37,7 @@ int main(){
     int n;//站点数 
     int rent;//租金
     fin>>n;
-    for (int i=0;i<n-1;i++){ //i 行，j 列 
+    for (int i=0;i<n-1;i++){ //二维向量，不满的值用0填满。
         list.push_back(line);
         for(int j=0;j<=i;j++){
             list[i].push_back(0);
